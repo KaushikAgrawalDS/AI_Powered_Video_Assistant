b378e0fe21a4e4f871e244314511764d2d16732d
KaushikAgrawalDS <agrawalkaushik45@gmail.com>
2026-08-02 00:01:22 +0530

    Fix protobuf version

--- Utils/Audio_processing.py
+++ Utils/Audio_processing.py
@@ -2,6 +2,7 @@ import yt_dlp
 import os
 from pydub import AudioSegment
 import traceback
+import subprocess
 
 DOWNLOAD_DIR = 'downloades'
 os.makedirs(DOWNLOAD_DIR,exist_ok=True)
@@ -26,13 +27,13 @@ def download_youtube_audio(url:str) -> str:
     return filename
 
 def convert_to_wav(input_path: str) -> str:
-    
-    """Convert any audio/video file to WAV format using pydub."""
     output_path = os.path.splitext(input_path)[0] + "_converted.wav"
-    audio = AudioSegment.from_file(input_path)
-    audio = audio.set_channels(1).set_frame_rate(16000)
-    audio.export(output_path,format='wav') 
-    return output_path 
+    subprocess.run([
+        "ffmpeg", "-y", "-i", input_path,
+        "-vn", "-ar", "16000", "-ac", "1",
+        output_path
+    ], check=True)
+    return output_path
 
 def chunk_audio(wav_path: str,chunk_minute: int =10) -> list:
     audio = AudioSegment.from_wav(wav_path)
